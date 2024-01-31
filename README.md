8c28369621fb3500849a39558f9b4433922f85bd
Greg Chetcuti <greg@chetcuti.com>
2024-01-30 22:50:00 -0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,8 @@
+Domain WHOIS Lookup
+-------------------
+
+## Installation
+
+- Clone the repository
+- Run "docker-compose up -d"
+- Load https://IP_ADDRESS:8094 in your web browser
---
+++ services/web/project/__init__.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+import whois
+from flask import Flask, render_template, request
+from time import sleep
+
+app = Flask(__name__, static_folder="assets")
+
+site_title = "Domain WHOIS Lookup"
+
+
+@app.route("/<domain>", methods=["GET"])
+def home_get(domain):
+    sleep(0.25)
+
+    page_body = process_domain(domain)
+
+    return display_homepage(domain, page_body)
+
+
+@app.route("/", methods=["GET"])
+def home():
+    return home_get("example.com")
+
+
+@app.route("/", methods=["POST"])
+def home_post():
+    domain = "example.com"
+
+    sleep(0.25)
+
+    submitted_domain = str(request.form["domain"])
+
+    if submitted_domain == "":
+        submitted_domain = domain
+
+    page_body = process_domain(submitted_domain)
+
+    return display_homepage(submitted_domain, page_body)
+
+
+def process_domain(domain):
+    page_body = ""
+
+    try:
+        domain_info = str(whois.whois(domain))
+        domain_info = domain_info.replace("\n", "<BR>")
+
+        page_body += domain_info
+
+    except Exception:
+        page_body += "Unable to perform domain WHOIS lookup, please try again."
+
+    return page_body
+
+
+def display_homepage(domain, page_body):
+    return render_template(
+        "home.html", site_title=site_title, domain=domain, page_body=page_body
+    )
+
+
+if __name__ == "__main__":
+    app.run()
